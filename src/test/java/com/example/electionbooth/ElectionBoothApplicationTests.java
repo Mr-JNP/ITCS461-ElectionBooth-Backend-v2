e2996f24bcafab5328c881727b81a91bb244b06c
Nontapat Pintira <jokenon@Nontapats-MacBook-Pro.local>
2020-04-21 16:07:37 +0700

    Created models

---
+++ src/test/java/com/example/electionbooth/ElectionBoothApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.electionbooth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectionBoothApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
